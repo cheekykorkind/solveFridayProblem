448f8d56a2f24617c6d53b0a7403bc61ecf4de51
IEUser <cheekorkind@gmail.com>
2019-01-29 07:12:46 -0800

    view, model

--- gamble/src/main/java/com/jiho/app/App.java
+++ gamble/src/main/java/com/jiho/app/App.java
@@ -1,5 +1,7 @@
 package com.jiho.app;
-import javax.swing.JFrame;
+
+import model.*;
+import view.*;
 
 /**
  * Hello world!
@@ -9,9 +11,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        JFrame f = new JFrame("스윙 시작");
-        f.setSize(300, 200);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setVisible(true);
+        new MainFrame();
     }
 }
